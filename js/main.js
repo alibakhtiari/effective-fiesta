105d209970d193358ddace8e9c45662f0d84e060
alibakhtiari <ali.bakhtiarii@gmail.com>
2021-04-10 02:05:58 +0430

    update the whole thing and add so many thing

--- js/main.js
+++ js/main.js
@@ -1,94 +1,140 @@
-/* mobile menu */
-var mt = document.querySelector('.menu-trigger');
-var nav = document.querySelector('.nav_list_mobile');
-var ham = document.querySelector('#hamburger_menu');
-
-mt.onclick = function() {
-  nav.classList.toggle('visible');
-  ham.classList.toggle('open');
+window.addEventListener('load', (event) => {
+	console.log('Page is fully loaded!');
+  });
+//get author name from meta
+document.querySelector('.designer_name').innerText = document.querySelector('meta[name="author"]').content;
+// active about menu link
+var activeMenu = document.querySelector('.active'),
+	aboutMenu = document.querySelector('.about'),
+	isHome = false;
+if (document.getElementById('home')) {
+	isHome = true;
+} else {
+	isHome = false
 }
-/* footer BOM get last modified */
-var last_mod = document.querySelector('.last_mod');
-var bom_mod = document.lastModified;
+if (location.hash == '#about' && isHome) {
+	activeMenu.classList.remove('active');
+	aboutMenu.classList.add('active')
+}
+//mobile menu
+var mt = document.querySelector('.menu-trigger'),
+	nav = document.querySelector('.nav_list_mobile'),
+	ham = document.querySelector('#hamburger_menu');
+
+mt.onclick = function () {
+	nav.classList.toggle('visible');
+	ham.classList.toggle('open');
+}
+//footer BOM get last modified
+var last_mod = document.querySelector('.last_mod'),
+	bom_mod = document.lastModified;
 last_mod.innerHTML = 'last updated or modified : ' + bom_mod;
-/* modal window */
-var modal = document.querySelector('#modal');
-modal.onclick = function (){
-  modal.style.display = 'none';
-  document.body.style.overflowY ='auto';
+// modal window
+var cButton = document.querySelector('.covid_modal'),
+	modal = document.querySelector('#modal');
+cButton.onclick = function () {
+	modal.style.display = 'block';
+	document.body.style.overflowY = 'hidden';
+}
+modal.onclick = function () {
+	modal.style.display = 'none';
+	document.body.style.overflowY = 'auto';
 }
 
-/*smooth scroll*/
-document
-    .querySelectorAll('.nav__item a[href^="#"]')
-    .forEach(trigger => {
-        trigger.onclick = function(e) {
-            e.preventDefault();
-            let hash = this.getAttribute('href');
-            let target = document.querySelector(hash);
-            let headerOffset = 100;
-            let elementPosition = target.offsetTop;
-            let offsetPosition = elementPosition - headerOffset;
+// form validation
+var vName, vEmail, vMessage = false;
 
-            window.scrollTo({
-                top: offsetPosition,
-                behavior: "smooth"
-            });
-        };
-    });
-    //html { scroll-behavior: smooth; }
+function validateName() {
+	var fName = document.querySelector('#name');
+	if (fName.value.trim() === "") {
+		this.setStatus(fName, 'Name cannot be blank', "error");
+		vName = false;
+	} else {
+		this.setStatus(fName, null, "success");
+		vName = true;
+	}
+};
+
+function validateEmail() {
+	var f_EM = document.querySelector('#email');
+	var regex = /\S+@\S+\.\S+/;
+	if (regex.test(f_EM.value)) {
+		this.setStatus(f_EM, null, "success");
+		vEmail = true;
+	} else {
+		this.setStatus(f_EM, "Please enter valid email address", "error");
+		vEmail = false;
+	}
+};
+
+function validateTextArea() {
+	var f_TA = document.querySelector('#message');
+	if (f_TA.value.trim() === "") {
+		this.setStatus(f_TA, 'Message cannot be blank', "error");
+		vMessage = false;
+	} else {
+		this.setStatus(f_TA, null, "success");
+		vMessage = true;
+	}
+};
+
+function setStatus(field, message, status) {
+	var successIcon = field.parentElement.querySelector('.icon-success'),
+		errorIcon = field.parentElement.querySelector('.icon-error'),
+		errorMessage = field.parentElement.querySelector('.error-message');
+
+	if (status === "success") {
+		if (errorIcon) {
+			errorIcon.classList.add('hidden');
+		}
+		if (errorMessage) {
+			errorMessage.innerText = "";
+		}
+		successIcon.classList.remove('hidden');
+		field.classList.remove('input-error');
+		field.classList.add('input-success');
+	}
+
+	if (status === "error") {
+		if (successIcon) {
+			successIcon.classList.add('hidden');
+		}
+		field.parentElement.querySelector('.error-message').innerText = message;
+		errorIcon.classList.remove('hidden');
+		field.classList.add('input-error');
+		field.classList.remove('input-success');
+	}
+	if (vName && vEmail && vMessage){
+		alert('All enteries are validated')
+	}
+}
 
 
+// Countdown
 
-    /* form validation */
-    var vName,vEmail,vMessage;
-    function validateName() {
-        var  fName = document.querySelector('#name');
-        if (fName.value.trim() === "") {
-          this.setStatus(fName, 'Name cannot be blank', "error");
-          vName = true;
-        } else {
-          this.setStatus(fName, null, "success");
-        }
-    };
-    function validateEmail() {
-        var  f_EM = document.querySelector('#email');
-          const regex = /\S+@\S+\.\S+/;
-          if (regex.test( f_EM.value)) {
-            this.setStatus( f_EM, null, "success");
-            vEmail = true;
-          } else {
-            this.setStatus( f_EM, "Please enter valid email address", "error");
-          }
-    };
-    function  validateTextArea() {
-        var  f_TA = document.querySelector('#message');
-        if ( f_TA.value.trim() === "") {
-          this.setStatus( f_TA, 'Message cannot be blank', "error");
-          vMessage = true;
-        } else {
-          this.setStatus( f_TA, null, "success");
-        }
-    };
-    function setStatus(field, message, status) {
-        const successIcon = field.parentElement.querySelector('.icon-success');
-        const errorIcon = field.parentElement.querySelector('.icon-error');
-        const errorMessage = field.parentElement.querySelector('.error-message');
-    
-        if (status === "success") {
-          if (errorIcon) {errorIcon.classList.add('hidden');}
-          if (errorMessage) {errorMessage.innerText = "";}
-          successIcon.classList.remove('hidden');
-          field.classList.remove('input-error');
-          field.classList.add('input-success');
-        }
-    
-        if (status === "error") {
-          if (successIcon) {successIcon.classList.add('hidden');}
-          field.parentElement.querySelector('.error-message').innerText = message;
-          errorIcon.classList.remove('hidden');
-          field.classList.add('input-error');
-          field.classList.remove('input-success');
-        }
-      }
-    
+// Predefined date for next branch openning
+var dDate = new Date("Nov 9, 2021 8:30:00").getTime();
+var countdownDiv = document.querySelector(".countdown");
+var x = setInterval(function () {
+	// Get the number of milliseconds since midnight, January 1, 1970. 
+	var dNow = new Date().getTime();
+	// Calculate date difference
+	var diff = dDate - dNow;
+	// Calculations for days, hours, minutes and seconds
+	var cSeconds = 1000;
+	var cMinutes = 1000 * 60;
+	var cHours = cMinutes * 60;
+	var cDays = cHours * 24;
+	var daysLeft = Math.round(diff / cDays);
+	var hoursLeft = Math.round((diff % cDays) / cHours);
+	var minutesLeft = Math.round((diff % cHours) / cMinutes);
+	var secondsLeft = Math.round((diff % cMinutes) / cSeconds);
+
+	// Output the result in an element with id="demo"
+	countdownDiv.innerHTML = "<h2>Time Left To Our Next Branch Openning</h2><span class='days'>" + daysLeft + "</span><span class='hours'>" + hoursLeft + "</span><span class='minutes'>" + minutesLeft + "</span><span class='seconds'>" + secondsLeft + "</span>";
+
+	// If the count down is over, write some text 
+	if (diff < 0) {
+
+	}
+}, 1000);
--- thingies/building-dashboards-using-grid-and-flex-box/README.markdown
+++
@@ -1,5 +0,0 @@
-# Building dashboards using grid and flex-box
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/kevjose/pen/YzXrobv](https://codepen.io/kevjose/pen/YzXrobv).
-
-
